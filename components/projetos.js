45abe741d9120ffde4f4f0106e865ce1b37517af
EudesNilton <eudes258@gmail.com>
2024-05-02 12:35:44 -0300

    finalização do componente dos projetos

--- components/projetos.js
+++ components/projetos.js
@@ -1,26 +1,36 @@
-  // Função para criar um card
-  function criarCard(titulo, conteudo, imagemSrc) {
-    // Criar elementos HTML
-    var card = document.createElement("div");
-    card.classList.add("card");
-    var tituloElemento = document.createElement("h2");
-    var conteudoElemento = document.createElement("p");
-    var imagemElemento = document.createElement("img");
 
-    // Configurar conteúdo dos elementos
-    tituloElemento.textContent = titulo;
-    conteudoElemento.textContent = conteudo;
-    imagemElemento.src = imagemSrc;
+var projetos = [
+  {
+    imgSrc: "https://s2.glbimg.com/uKmGy-25FnmER9NB_X0-WV-2if8=/s.glbimg.com/og/rg/f/original/2013/07/19/v72k2z45.jpg",
+    titulo: "x",
+    descricao: "blablabla",
+    tecnologias: `html css javascript`,
+  },
+]
 
-    // Adicionar elementos ao card
-    card.appendChild(tituloElemento);
-    card.appendChild(conteudoElemento);
-    card.appendChild(imagemElemento);
+const cardContainer = document.getElementById("containerDeCards");
 
-    // Adicionar o card ao container na página
-    var container = document.getElementById("cardContainer");
-    container.appendChild(card);
-  }
+const criarProjetoElemento = projeto => {
+  const novoCard = document.createElement("div");
+  novoCard.classList.add("card");
 
-  // Chamada da função para criar um card
-  criarCard("Título do Card", "Este é o conteúdo do card.", "https://via.placeholder.com/300"); // exemplo de URL de imagem
+  novoCard.innerHTML = `
+    <div>
+      <img src="${projeto.imgSrc}" alt="Imagem do Projeto">
+      <button>code</button>
+      <button>view</button>
+    </div>
+    <img src="" alt="">
+    <h2>${projeto.titulo}</h2>
+    <div>
+      <p>${projeto.descricao}</p>
+      <span>${projeto.tecnologias}</span>
+    </div>
+  `;
+
+  return novoCard;
+};
+
+projetos.forEach(projeto => {
+  cardContainer.appendChild(criarProjetoElemento(projeto));
+});
